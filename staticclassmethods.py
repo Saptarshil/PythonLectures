6f89a5802145017e15c873879f56113b6279f692
sap <b517036@iiit-bh.ac.in>
2023-04-24 17:03:55 +0530

    class methods

--- staticclassmethods.py
+++ staticclassmethods.py
@@ -1,17 +1,25 @@
 from datetime import date
 
+
 class Person:
-    def __init__(self, name, age): #self holds the object reference.
+    def __init__(self, name, age):  # self holds the current object reference.
         self.name = name
         self.age = age
 
-    @classmethod #on using this annotation, we transfer the ownership of the method to the class instead of the object.
-    def frombirthyear(cls, name, birthyear): #cls is a parameter that holds the class object.
-        return cls(name, date.today().year-birthyear) #here we are returning the reference for the class object which can be stored in a variable.
+    @classmethod  # on using this annotation, we transfer the ownership of the method to the class instead of the object.
+    def frombirthyear(cls, name, birthyear):
+        print(cls) # cls is a parameter that holds the class reference.
+        return cls(name, date.today().year - birthyear)  # new object is being created to be stored in a variable.
 
     def display(self):
-        print(self.name + "'s age is: "+str(self.age))
+        print(self)
+        print(self.name + "'s age is: " + str(self.age))
 
 
-person = Person("Happy", 23) #person holds reference to the object which has a name happy and age 23.
+person = Person("Happy", 23)  # person holds reference to the object which has a name happy and age 23.
 person.display()
+person2 = Person.frombirthyear("Laughy", 2006)  # you can also call it using the class name because it is a class method.
+person2.display()
+
+#  we have 2 methods tp enter the age. Either enter the age directly or enter your birth year. Hence we have 2 different methods to accomplish the same task in 2 different ways.
+#  cls holds a new object everytime. hence we can approach a problem in multiple ways using class methods.
